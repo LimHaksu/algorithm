1e9fc10019ad32ede1aa00ac572ff0c39cd9c092
Lim Haksu <neroren2@gmail.com>
2021-05-06 16:35:18 +0900

    프로그래머스 파이썬 2019 카카오 개발자 겨울 인턴십 4번

--- programmers/python/2019_카카오_개발자_겨울_인턴십/4.py
+++ programmers/python/2019_카카오_개발자_겨울_인턴십/4.py
@@ -1,29 +1,42 @@
-import sys
-sys.setrecursionlimit(10**6)
+# import sys
+# sys.setrecursionlimit(10**6)
+# def solution(k, room_number):
+#     answer = []
+#     p = {}
+#     for num in room_number:
+#         p_num = find(p, num)
+#         answer.append(p_num)
+#         union(p, p_num, p_num+1)
+#     return answer
+
+# def find(p, a):
+#     if a in p:
+#         pa = p[a]
+#     else:
+#         return a
+#     p[a] = find(p, pa)
+#     return p[a]
+
+# def union(p, a, b):
+#     pa = find(p, a)
+#     pb = find(p, b)
+#     if pa > pb:
+#         p[b] = pa
+#     elif pb > pa:
+#         p[a] = pb
+
 def solution(k, room_number):
-    answer = []
-    p = {}
-    for num in room_number:
-        p_num = find(p, num)
-        answer.append(p_num)
-        union(p, p_num, p_num+1)
-    return answer
+    room_dic = {}
+    ret = []
+    for n in room_number:
+        visit = [n]
+        while n in room_dic:
+            n = room_dic[n]
+            visit.append(n)
+        ret.append(n)
+        for j in visit:
+            room_dic[j] = n+1
+    return ret
 
-def find(p, a):
-    if a in p:
-        pa = p[a]
-    else:
-        return a
-    p[a] = find(p, pa)
-    return p[a]
-
-def union(p, a, b):
-    pa = find(p, a)
-    pb = find(p, b)
-    if pa > pb:
-        p[b] = pa
-    elif pb > pa:
-        p[a] = pb
-
-# print(solution(pow(10,12), [i for i in range(pow(10,12)-200000, pow(10,12))]))
-print(solution(	10, [6,5,4,3,2,1,1]))
+solution(pow(10,12), [1 for i in range(200000)])
+# print(solution(	10, [6,5,4,3,2,1,1]))
